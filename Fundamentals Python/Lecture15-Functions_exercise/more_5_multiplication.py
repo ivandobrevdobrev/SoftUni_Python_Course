a7d422e9d1a3a4947d85ac40d09c7885b2c332e5
ivandobrevdobrev <ivandobrev768@gmail.com>
2023-10-12 01:10:32 +0300

    Upload of Functions-More exercises

--- Fundamentals Python/Lecture15-Functions_exercise/more_5_multiplication.py
+++ Fundamentals Python/Lecture15-Functions_exercise/more_5_multiplication.py
@@ -1,15 +1,33 @@
-def multiplication_sign(num1, num2, num3: int):
-    if num1 == 0 or num2 == 0 or num3 == 0:
-        return "zero"
-    elif num1 < 0 or num2 < 0 or num3 < 0:
-        return "negative"
+# def multiplication_sign(num1, num2, num3: int):
+#     if num1 == 0 or num2 == 0 or num3 == 0:
+#         return "zero"
+#     elif num1 < 0 or num2 < 0 or num3 < 0:
+#         return "negative"
+#     else:
+#         return "positive"
+#
+#
+# n1 = int(input())
+# n2 = int(input())
+# n3 = int(input())
+#
+# result = multiplication_sign(n1, n2, n3)
+# print(result)
+
+def find_product_sign(a, b, c):
+    # Check the number of negative values
+    num_negatives = sum(1 for num in (a, b, c) if num < 0)
+
+    if num_negatives % 2 == 0:
+        return "Positive"
     else:
-        return "positive"
+        return "Negative"
 
 
-n1 = int(input())
-n2 = int(input())
-n3 = int(input())
+# Input three numbers from the user
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = float(input("Enter the third number: "))
 
-result = multiplication_sign(n1, n2, n3)
-print(result)
+result = find_product_sign(a, b, c)
+print(f"The product of {a}, {b}, and {c} is {result}.")
